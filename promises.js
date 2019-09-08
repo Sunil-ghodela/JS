bd31e5b9e1af3c2cf6921444684129838b3c022b
Sunil-ghodela <35575602+Sunil-ghodela@users.noreply.github.com>
2019-09-08 15:31:24 +0530

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,1 +1,21 @@
 
+const getVal = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(()=> {
+      let error = true
+      if (!error) {
+        resolve({'val': 'Sunil'})
+      } else {
+        reject('le ja')
+      }
+    })
+  })
+}
+
+getVal()
+  .then(item => {
+    console.log(item.val)
+})
+  .catch(error => {
+    console.log(error)
+  })
